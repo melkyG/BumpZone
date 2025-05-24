8ed0952e3383c54b06f4691f70adc37036208cce
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-05-23 22:54:58 -0400

    rebuild

--- server/public/flutter_service_worker.js
+++ server/public/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "04886eb7a84d8eef2180c6571b683f36",
+"flutter_bootstrap.js": "c67391e1668392a167dab1eac479a21b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
