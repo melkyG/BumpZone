f27ee1982379b2345b0d45be1e29cbabbe775a54
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-01 22:27:33 -0400

    diff band init settings

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "76378076"
+    serviceWorkerVersion: "3456792065"
   }
 });
