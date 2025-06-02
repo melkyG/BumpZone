ff133af17f98edb54ed2fbe13e2848509a26f69b
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-01 22:33:35 -0400

    undo band settings

--- server/game/state.js
+++ server/game/state.js
@@ -5,10 +5,10 @@ const ACCELERATION = 600; // units per second^2
 
 const BALL_RADIUS = 18; // must match client
 
-const BAND_SEGMENTS_PER_SIDE = 25; // from reference code
-const BAND_SPRING_CONSTANT = 2900;
-const BAND_DAMPING_COEFF = 0.08;
-const BAND_MASS = 0.06;
+const BAND_SEGMENTS_PER_SIDE = 35; // from reference code
+const BAND_SPRING_CONSTANT = 10.0;
+const BAND_DAMPING_COEFF = 1.0;
+const BAND_MASS = 1.0;
 const BAND_REST_LENGTH_SCALE = 0.35;
 const BAND_COEFFICIENT_OF_RESTITUTION = 0.85;
 
