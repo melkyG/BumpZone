2b8678bc13b22c478fa01430e100791060a3b069
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-02 23:01:22 -0400

    fixed input ref, resize

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "980795283"
+    serviceWorkerVersion: "4213422599"
   }
 });
