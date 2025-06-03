aeace7aacc2c42b824384f63661b63a28b60ef40
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-02 23:48:46 -0400

    rebuild

--- server/game/state.js
+++ server/game/state.js
@@ -26,7 +26,7 @@ class GameState {
 
     // --- Elastic Zone Data Structures ---
     // Four posts at the corners of a square
-    const margin = 100;
+    const margin = 250;
     this.posts = [
       { x: margin, y: margin },
       { x: ARENA_SIZE - margin, y: margin },
