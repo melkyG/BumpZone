2f13563345d8ac90d1da4f08f505d3ad329ea5ff
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-02 22:43:37 -0400

    rebuild

--- client/lib/screens/game.dart
+++ client/lib/screens/game.dart
@@ -27,7 +27,7 @@ class _GameScreenState extends State<GameScreen> {
   List<Player> _players = [];
   List<Band> _bands = [];
   List<BandSegment> _posts = [];
-  double _arenaLogicalSize = 2500.0; // Match server ARENA_SIZE
+  double _arenaLogicalSize = 1500.0;
 
   // Band settings state (add these fields)
   double _springConstant = 10.0;
@@ -323,7 +323,7 @@ class _ArenaPainter extends CustomPainter {
       size.height / 2 - camY * scale,
     );
 
-    // Draw arena border (make sure it matches the logical arena size, not the scaled widget size)
+    // Draw arena border
     final Paint borderPaint = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.stroke
