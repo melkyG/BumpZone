1121cfeabf94847cc88621ccb9f95a65789667a6
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-02 23:03:56 -0400

    resize

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4213422599"
+    serviceWorkerVersion: "3149798604"
   }
 });
--- server/public/flutter_service_worker.js
+++ server/public/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ed88d0100bf2de6d98e7d7d9a741b5af",
+"flutter_bootstrap.js": "3db8632aa51778fb8b65d937fb709d78",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
