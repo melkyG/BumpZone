10f2f2a461107464d6977a240d1c34fb47a7dfe2
LAPTOP-2673EB1N\gmelk <gmelkuev@gmail.com>
2025-06-04 02:10:10 -0400

    restore good commit

--- server/public/flutter_bootstrap.js
+++ server/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "158037047"
+    serviceWorkerVersion: "2457665784"
   }
 });
